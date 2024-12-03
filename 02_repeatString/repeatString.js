a2f32ba5fe36fb33accaf6f76ecf963759d1e49b
joaopaulorrt <joaopaulorrt@gmail.com>
2024-12-02 22:48:54 -0300

    Exercises 2 and 3 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, repetitions) {
+    
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+    
+    let result = ''
+    for (let i = 0; i < repetitions ; i++) {
+        result += text
+    }
+    return result;
 };
 
 // Do not edit below this line
