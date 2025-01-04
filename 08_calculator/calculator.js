1e36408ad611e10719866ed4ec582129d62e1986
joaopaulorrt <joaopaulorrt@gmail.com>
2025-01-03 21:55:39 -0300

    Exerício 8 finalizado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let factorial = 1;
+    for (let i = 1; i <= a ; i++) {
+        factorial *= i;
+    }
+    return factorial;
 };
 
 // Do not edit below this line
